31c4f8fd896dbeb550d3b6dc353c7c596705daed
haythamdahri <haytham.dahri@gmail.com>
2020-03-26 12:04:12 +0100

    Fix updated_at field

--- todos/models.py
+++ todos/models.py
@@ -7,5 +7,5 @@ from django.utils.timezone import now
 class Todo(models.Model):
     title = models.CharField(blank=False, null=False, max_length=255)
     content = models.TextField(blank=False, null=False, max_length=5600)
-    created_at = models.DateTimeField(auto_now=True, verbose_name="Created At")
+    created_at = models.DateTimeField(verbose_name="Created At")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
